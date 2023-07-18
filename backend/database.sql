bd840931fca665f9742fec5954c247fde9813eca
Guillaume Lamourec <parkerpool@live.fr>
2023-07-18 15:52:36 +0200

    cleaned the template, installed base modules and prepared routes and database

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,31 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `vegetable`
+-- -----------------------------------------------------
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+CREATE TABLE IF NOT EXISTS `vegetable` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `vegetable` VARCHAR(80) NOT NULL,
+  `variety` VARCHAR(80) NOT NULL,
+  `price` DECIMAL(4,2) NOT NULL,
+  `comments` VARCHAR(255) NULL,
+  `picture` VARCHAR(255) NULL,
+  `isAvailable` BOOLEAN NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+INSERT INTO vegetable (vegetable, variety, price, comments, picture, isAvailable) VALUES 
+("Carotte", "Des sables", 4.20, "Bien sucrées et iodées", "https://images.unsplash.com/photo-1598170845058-32b9d6a5da37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80", 1);
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pseudo` VARCHAR(45) NOT NULL,
+  `hashed_password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+INSERT INTO user (pseudo, hashed_password) VALUES
+("admin", "$argon2id$v=19$m=16,t=2,p=1$d2Q4bzVpdWNzODAwMDAwMA$1ASIKBBRBInUbEFqZjMX+g")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import Home from "./pages/Home";
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        {/* --- Routes publiques --- */}
+        <Route path="/" />
+        <Route path="/vegetables" />
+        <Route path="/gallery" />
+        <Route path="/admin" />
+      </Routes>
     </div>
   );
 }
