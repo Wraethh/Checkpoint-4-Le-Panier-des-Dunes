9b08b44479a5284d17dcf8c828fdaa4c5cf1858a
Guillaume Lamourec <parkerpool@live.fr>
2023-07-19 00:37:16 +0200

    logout working

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,6 +1,5 @@
-import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 import APIService from "../services/APIService";
 
 const UserContext = createContext();
@@ -11,11 +10,6 @@ export function UserContextProvider({ children }) {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user") || "{}")
   );
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user.id) navigate("/");
-  }, [user.id]);
 
   const login = (_user) => {
     setUser(_user);
